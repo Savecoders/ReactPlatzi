216ebc6ef9cbbc62f8d02d7d5d5365017141566b
Savecoders <pablopincay2003@gmail.com>
2023-01-10 20:51:20 -0500

    fix icons in recoverypass

--- src/pages/RecoveryPass.tsx
+++ src/pages/RecoveryPass.tsx
@@ -1,21 +1,25 @@
-import "../styles/RecoveryPass.css";
-
+import "@/styles/RecoveryPass.css";
+import emailIcon from "@/assets/icons/email.svg";
+import logoYardSale from "@/assets/logos/logo_yard_sale.svg";
+import { Link } from "react-router-dom";
 const RecoveryPass = () => {
   return (
     <div className="login">
       <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+        <img src={logoYardSale} alt="logo" className="logo" />
         <h1 className="title">Email has been sent!</h1>
         <p className="subtitle">
           Please check your inbox for instructions on how to reset the password
         </p>
         <div className="email-image">
-          <img src="./icons/email.svg" alt="email" />
+          <img src={emailIcon} alt="email" />
         </div>
-        <button className="primary-button login-button">Login</button>
+        <Link to="/" className="primary-button login-button">
+          <button className="primary-button ">Login</button>
+        </Link>
         <p className="resend">
           <span>Didn't receive the email?</span>
-          <a href="/">Resend</a>
+          <Link to="/SendEmail">Resend</Link>
         </p>
       </div>
     </div>
